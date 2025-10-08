aa7f19fb4a678f8c403fbe6d3f12748f8c87a9d4
Abhi <576abhishek.2020@gmail.com>
2025-10-08 17:47:59 +0530

    neq

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -54,6 +54,13 @@ const Navbar = () => {
 
       {/* Mobile Navigation Menu */}
       <div className={`mobile-menu ${isMenuOpen ? 'active' : ''}`}>
+        <div className="mobile-menu-header">
+          <button className="mobile-menu-close" onClick={toggleMenu}>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </button>
+        </div>
         <ul className="mobile-menu-list">
           <li className="mobile-menu-item">
             <a href="/" className="mobile-menu-link" onClick={toggleMenu}>Home</a>
