fed6da44b096f3ea103cd55e787f41a378605d26
Abhi <576abhishek.2020@gmail.com>
2025-10-09 14:31:21 +0530

    new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -124,7 +124,7 @@ const Contact = () => {
 
                 <div className="form-group">
                   <div className="form-input-wrapper">
-                    <svg className="form-icon" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="form-icon1" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM20 16H5.17L4 17.17V4H20V16Z" fill="#6B7280"/>
                     </svg>
                     <textarea id="message" name="message" rows="4" placeholder="Message" required></textarea>
